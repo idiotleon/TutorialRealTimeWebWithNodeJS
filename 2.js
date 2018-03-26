766ddb00b8271b96864b61e23ef826a0eab2a6f0
Yang Lu <yang.lu3@huawei.com>
2018-03-26 11:37:16 -0700

    demo example: File IO and Modules
    Module 2.4

---
+++ 2.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+function printHelp() {
+    console.log("1.js (c) Leon");
+    console.log("");
+    console.log("usage:");
+    console.log("--help                 print this help");
+    console.log("--file={NAME}                  read the file of {NAME} and output");
+    console.log("");
+}
+
+var args = require('minimist')(process.argv.slice(2), { string: "file" });
+
+if (args.help || !args.file) {
+    printHelp();
+    process.exit(1);
+}
+
+var hello = require('./helloworld.js');
+
+var contents = hello.say(args.file);
+
+console.log(contents.toString());
