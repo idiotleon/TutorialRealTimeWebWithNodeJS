9ae2a9a6bbf831a4071afbdf568fcef1c2c1c14c
Yang Lu <yang.lu3@huawei.com>
2018-03-26 11:59:55 -0700

    demo example: Asynchronous File IO
    Module 2.5
    
    The program waits 1s before it prints out the result

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,15 @@
 function say(filename, cb) {
-    return fs.readFile(filename, cb);
+    return fs.readFile(filename, function (err, contents) {
+        if (err) {
+            cb(err);
+        } else {
+            // The program waits 1s before it prints out the result
+            setTimeout(function () {
+                // no error to be passed
+                cb(null, contents);
+            }, 1000);
+        }
+    });
 }
 
 var fs = require("fs");
