949639fce943fa5d3f09b729aa409681120daf9e
Yang Lu <yang.lu3@huawei.com>
2018-03-26 11:18:24 -0700

    demo example: Adding Help
    Module 2.3

--- 1.js
+++ 1.js
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
 
+function printHelp() {
+    console.log("1.js (c) Leon");
+    console.log("");
+    console.log("usage:");
+    console.log("--help             print this help");
+    console.log("--name             say hello to {NAME}");
+    console.log("");
+}
+
 var args = require('minimist')(process.argv.slice(2), { string: "name" });
+
+if (args.help || !args.name) {
+    printHelp();
+    process.exit(1);
+}
 
 var name = args.name;
 
