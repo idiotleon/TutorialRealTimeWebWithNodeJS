9c8c3df03339a8124e5bc021692afdd87e2647ec
Yang Lu <yang.lu3@huawei.com>
2018-03-27 11:50:00 -0700

    demo example: Using Asynquence
    Module 2.6

---
+++ 3.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+function printHelp() {
+    console.log("1.js (c) Leon");
+    console.log("");
+    console.log("usage:");
+    console.log("--help                 print this help");
+    console.log("--file={NAME}    read the file of {NAME} and output");
+    console.log("");
+}
+
+var args = require('minimist')(process.argv.slice(2), { string: "file" });
+
+if (args.help || !args.file) {
+    printHelp();
+    process.exit(1);
+}
+
+var hello = require('./helloworld.js');
+
+hello.say(args.file, function (err, contents) {
+    if (err) {
+        console.error("Error: " + err);
+    } else {
+        console.log(contents.toString());
+    }
+});
