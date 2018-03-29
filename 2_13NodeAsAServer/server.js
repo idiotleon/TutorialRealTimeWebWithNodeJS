24aaf494a8a2cc085b37b34a10feda9011e34380
Yang Lu <yang.lu3@huawei.com>
2018-03-29 11:23:24 -0700

    demo example: Node as a Webserver
    Module 2.13

---
+++ 2_13NodeAsAServer/server.js
@@ -0,0 +1,12 @@
+function handleHTTP(req, res) {
+    res.end("Hello World: " + Math.random());
+}
+
+var host = 'localhost';
+var port = 8006;
+
+var http = require("http");
+var http_server = http
+    .createServer(handleHTTP)
+    .listen(port, host);
+
