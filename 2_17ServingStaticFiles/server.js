316d1878c9c8eb6f65b54b707a891f2fe64973b8
Yang Lu <yang.lu3@huawei.com>
2018-03-29 15:31:54 -0700

    demo example: Serving Static Files
    Module 2.17

---
+++ 2_17ServingStaticFiles/server.js
@@ -0,0 +1,28 @@
+function handleHTTP(req, res) {
+    if (req.method === "GET") {
+        if (/^\/\d+(?=$|[\/?#])/.test(req.url)) {
+            req.addListener("end", function () {
+                req.url = req.url.replace(/^\/(\d+).*$/, "/$1.html");
+                static_files.serve(req, res);
+            });
+            req.resume();
+        } else {
+            res.writeHead(403);
+            res.end("Get outta here!");
+        }
+    } else {
+        res.writeHead(403);
+        res.end("Get outta here!");
+    }
+}
+
+var host = 'localhost';
+var port = 8006;
+
+var http = require('http');
+var http_server = http
+    .createServer(handleHTTP)
+    .listen(port, host);
+
+var node_static = require("node-static");
+var static_files = new node_static.Server(__dirname);
