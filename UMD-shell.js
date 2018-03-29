8dddda0478dfdbd0e53cb52671e5fba8f59d8c4a
Yang Lu <yang.lu3@huawei.com>
2018-03-28 18:47:39 -0700

    demo example: Extending Modules
    Module 2.11

---
+++ UMD-shell.js
@@ -0,0 +1,8 @@
+(funciton UMD(name, context, function){
+    if (typeof module !== "undefined" && module.exports) { module.exports = definition(); }
+    else if (typeof define === "function" && define.amd) { define(definition); }
+    else { context[name] = definition(name, context); }
+}) ("YourModuleName", this, function DEF(name, context) {
+    var publicAPI = { /*...*/ };
+    return publicAPI;
+});
